fa2a7285950ce983c250d26d780fa10425ec5e5f
Eddy Cedeño <miguepoke28@gmail.com>
2024-10-24 15:38:15 -0500

    fix file name inconsistencies

--- src/db/world-structure.ts
+++ src/db/world-structure.ts
@@ -1,4 +1,4 @@
 export const worldStructure = {
-  a: ["a1x1y1", "a2x1y1", "a3x1y1", "a4x1y1", "a4x1y2", "a4x1y3"],
-  b: ["b1x1y1", "b2x1y1", "b3x1y1", "b4x1y1", "b4x1y2", "b4x1y3"],
+  a: ["ax1y1"],
+  b: ["bx1y1"],
 };
---
+++ src/db/worlds/b/bx1y1.ts
@@ -0,0 +1,3 @@
+export const b1x1y1 = {
+  background: "src/assets/react.svg",
+};
