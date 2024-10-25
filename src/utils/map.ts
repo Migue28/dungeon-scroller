c1bccd0ca3ae5f1a7235b0c3d7cf448b5bf8fa51
Eddy Cedeño <miguepoke28@gmail.com>
2024-10-25 15:40:20 -0500

    change goDown and goLeft logic to not have negative numbers

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -18,17 +18,24 @@ export const mapIdConstructor = (id: string) => {
 
 export const mapCellConstructor = (mapId: string) => {
   const _mapId = mapIdConstructor(mapId);
+  const currentYCoordinate = _mapId.yCoordinate.coordinate;
+  const currentXCoordinate = _mapId.xCoordinate.coordinate;
+
   const mapCell: IMapCell = {
     mapId: _mapId,
     goUp: `${_mapId.world}${_mapId.xCoordinate.name}${
       _mapId.xCoordinate.coordinate
     }${_mapId.yCoordinate.name}${_mapId.yCoordinate.coordinate + 1}`,
+
     goDown: `${_mapId.world}${_mapId.xCoordinate.name}${
       _mapId.xCoordinate.coordinate
-    }${_mapId.yCoordinate.name}${_mapId.yCoordinate.coordinate - 1}`,
-    goLeft: `${_mapId.world}${_mapId.xCoordinate.name}${
-      _mapId.xCoordinate.coordinate - 1
-    }${_mapId.yCoordinate.name}${_mapId.yCoordinate.coordinate}`,
+    }${_mapId.yCoordinate.name}${Math.max(0, currentYCoordinate - 1)}`, // Ensure it doesn't go below 0
+
+    goLeft: `${_mapId.world}${_mapId.xCoordinate.name}${Math.max(
+      0,
+      currentXCoordinate - 1
+    )}${_mapId.yCoordinate.name}${currentYCoordinate}`, // Ensure it doesn't go below 0
+
     goRight: `${_mapId.world}${_mapId.xCoordinate.name}${
       _mapId.xCoordinate.coordinate + 1
     }${_mapId.yCoordinate.name}${_mapId.yCoordinate.coordinate}`,
