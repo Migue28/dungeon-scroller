8f06e21024f6b10c66eed1c1961f1da126641f14
Eddy Cedeño <miguepoke28@gmail.com>
2024-10-26 11:35:28 -0500

    remove props because now comes from hook

--- src/views/map-cell-view.tsx
+++ src/views/map-cell-view.tsx
@@ -2,13 +2,12 @@ import { useEffect } from "react";
 
 import { useGetMapCellContent } from "../hooks/use-get-map-cell";
 import { MapCell } from "../components/map-cell";
+import { usePlayer } from "../hooks/use-player";
+import { mapIdConstructor } from "../utils/map";
 
-type MapCellProps = {
-  world: string;
-  mapId: string;
-};
-
-export const MapCellView = ({ world, mapId }: MapCellProps) => {
+export const MapCellView = () => {
+  const { position } = usePlayer();
+  const { world, id: mapId } = mapIdConstructor(position);
   const { selectedMapCellContent } = useGetMapCellContent(world, mapId);
 
   // If you need to perform additional logic when `mapCellData` updates, use `useEffect`.
