803b1a1e500bae695716158097fa92c8a1040f7b
Eddy Cedeño <miguepoke28@gmail.com>
2024-10-26 11:35:52 -0500

    adjust PlayerProvider and MapCellView

--- src/views/map-view.tsx
+++ src/views/map-view.tsx
@@ -1,26 +1,26 @@
-import { Map } from "../components/map";
+// import { Map } from "../components/map";
 import { MapCanva } from "../components/map-canva";
-import { Player } from "../components/player";
-import { IMapCell } from "../types/map";
-import { mapCellConstructor } from "../utils/map";
+import { PlayerProvider } from "../providers/player-provider";
+// import { IMapCell } from "../types/map";
+// import { mapCellConstructor } from "../utils/map";
 import { MapCellView } from "./map-cell-view";
 
 export const MapView = () => {
-  const map: IMapCell[] = [
-    mapCellConstructor("ax1y1"),
-    mapCellConstructor("ax2y1"),
-    mapCellConstructor("ax3y1"),
-    mapCellConstructor("ax4y1"),
-    mapCellConstructor("ax4y2"),
-    mapCellConstructor("ax4y3"),
-  ];
+  // const map: IMapCell[] = [
+  //   mapCellConstructor("ax1y1"),
+  //   mapCellConstructor("ax2y1"),
+  //   mapCellConstructor("ax3y1"),
+  //   mapCellConstructor("ax4y1"),
+  //   mapCellConstructor("ax4y2"),
+  //   mapCellConstructor("ax4y3"),
+  // ];
 
   return (
-    <Player>
+    <PlayerProvider>
       <MapCanva>
         {/* <Map mapCells={map} /> */}
-        <MapCellView world="a" mapId="ax1y1" />
+        <MapCellView />
       </MapCanva>
-    </Player>
+    </PlayerProvider>
   );
 };
