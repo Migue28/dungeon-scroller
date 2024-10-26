55670ff9055e1c2cc9c88b9d9cb3018410edf817
Eddy Cedeño <miguepoke28@gmail.com>
2024-10-26 11:53:01 -0500

    change alt to file name

--- src/components/map-cell.tsx
+++ src/components/map-cell.tsx
@@ -7,12 +7,13 @@ type MapCellProps = {
 
 export const MapCell = ({ mapId, content }: MapCellProps) => {
   const imgUrl = content?.background || "";
+  const fileName = imgUrl.split("/").pop();
   return (
     <div className="relative w-full h-full">
       <img
         id={mapId}
         src={imgUrl}
-        alt={mapId}
+        alt={fileName}
         className="w-full h-full object-cover"
       />
       <p className="absolute top-0 left-0 p-2 text-white bg-black bg-opacity-50">
