20c3d27b912e5fc9c62e4d1e516b1d8833785d1c
Eddy Cedeño <miguepoke28@gmail.com>
2024-10-26 11:53:48 -0500

    remove not used useEffect

--- src/views/map-cell-view.tsx
+++ src/views/map-cell-view.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 import { useGetMapCellContent } from "../hooks/use-get-map-cell";
 import { MapCell } from "../components/map-cell";
 import { usePlayer } from "../hooks/use-player";
@@ -10,11 +8,5 @@ export const MapCellView = () => {
   const { world, id: mapId } = mapIdConstructor(position);
   const { selectedMapCellContent } = useGetMapCellContent(world, mapId);
 
-  // If you need to perform additional logic when `mapCellData` updates, use `useEffect`.
-  useEffect(() => {
-    if (selectedMapCellContent) {
-      // Perform any additional logic here when `mapCellData` is updated.
-    }
-  }, [selectedMapCellContent]);
   return <MapCell mapId={mapId} content={selectedMapCellContent} />;
 };
